4906e5648b17771d7878bfa744509ba5f4dca762
DoubleA4 <andyalam03@gmail.com>
2023-04-03 17:11:18 +0700

    initial commit

---
+++ praktikum 1/percobaan1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[255];
+    int age, telephone;
+    printf("Masukkan data diri anda\n");
+    printf("Nama: ");
+    scanf("%s", name);
+    printf("Umur: ");
+    scanf("%i", &age);
+    printf("No Telfon: ");
+    scanf("%i", &telephone);
+    printf("Hello %s, Umur %i, no telp %i\nBagaimana kabar anda hari ini?\n", name, age, telephone);
+}
