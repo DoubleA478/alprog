837d37dee75aeceffc5b1e5a83d477a13687085d
DoubleA478 <2040221078@mhs.its.ac.id>
2023-04-04 08:47:48 +0700

    percobaan 4 and refactoring

--- praktikum 1/percobaan2.c
+++ praktikum 1/percobaan2.c
@@ -2,11 +2,12 @@
 
 int main()
 {
-    int speed, time;
+    int speed, time, range;
     printf("Masukkan kecepatan dan waktu mobil anda\n");
     printf("Kecepatan (m/detik):");
     scanf("%i", &speed);
     printf("Waktu (detik):");
     scanf("%i", &time);
-    printf("Jadi jarak tempuh(m) anda adalah %i meter\n", speed * time);
+    range = speed * time;
+    printf("Jadi jarak tempuh(m) anda adalah %i meter\n", range);
 }
--- praktikum 1/percobaan4.c
+++ praktikum 1/percobaan4.c
@@ -2,4 +2,14 @@
 
 int main()
 {
+    int lenght, width, height, volume;
+    printf("Masukkan panjang, lebar dan tinggi\n");
+    printf("Panjang(cm):");
+    scanf("%i", &lenght);
+    printf("Lebar(cm):");
+    scanf("%i", &width);
+    printf("Tinggi(cm):");
+    scanf("%i", &height);
+    volume = lenght * width * height;
+    printf("jadi volumenya adalah %i cm kubik\n", volume);
 }
