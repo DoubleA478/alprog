e7a937044c81b9bcf94d6e9454c982f4acda8392
DoubleA4 <andyalam03@gmail.com>
2023-04-10 11:19:06 +0700

    p3p1

---
+++ praktikum 3/pendahuluan1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int loop = 1;
+    while (loop == 1)
+    {
+        int num, div, remain;
+        char choice;
+        printf("masukkan suatu bilangan\n");
+        scanf("%i", &num);
+        printf("masukkan bilangan pembagi(2/3/4/5)\n");
+        scanf("%i", &div);
+        printf("bilangan yang dipilih:%i\n", num);
+        printf("bilangan pembagi:%i", div);
+        if (num % div == 0)
+        {
+            printf("tidak ada\n");
+        }
+        else
+        {
+            printf("ada\n");
+        }
+        printf("apakah anda ingin meneruskan(Y/n) ");
+        scanf(" %c", &choice);
+        if (choice >= 'a' && choice <= 'z')
+        {
+            choice = choice - 32;
+        }
+        if (choice == 'N')
+        {
+            loop = 0;
+        }
+    }
+    return 0;
+}
