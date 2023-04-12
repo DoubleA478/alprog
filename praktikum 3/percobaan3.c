4d5d679a6f813a10151c39bde7a29e54d7d7dbcd
DoubleA478 <2040221078@mhs.its.ac.id>
2023-04-12 08:48:07 +0700

    seed

--- praktikum 3/percobaan3.c
+++ praktikum 3/percobaan3.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char const *argv[])
 {
     int random, guess;
+    srand(time(NULL));
     random = rand() % 100 + 1;
     while (guess != random)
     {
