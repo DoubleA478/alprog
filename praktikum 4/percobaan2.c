d294142621e4086244ea96065e75658949a90ffa
DoubleA478 <2040221078@mhs.its.ac.id>
2023-04-26 23:18:13 +0700

    flowchart life improvement

--- praktikum 4/percobaan2.c
+++ praktikum 4/percobaan2.c
@@ -2,11 +2,11 @@
 
 int main(int argc, char const *argv[])
 {
-    for (int row = 0; row < 3; row++)
+    for (int row = 1; row <= 3; row++)
     {
-        for (int col = 0; col < 5; col++)
+        for (int col = 1; col <= 5; col++)
         {
-            printf("%3i", (row + 1) * (col + 1));
+            printf("%3i", row * col);
         }
         printf("\n");
     }
