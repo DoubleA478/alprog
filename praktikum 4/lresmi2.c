c82a20b49de81a0426bb0f08af636bf271ec990d
DoubleA478 <2040221078@mhs.its.ac.id>
2023-04-29 08:06:09 +0700

    lmao

--- praktikum 4/lresmi2.c
+++ praktikum 4/lresmi2.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
 
-int isPrime(int number)
-{
-    for (int i = 2; i <= number / 2; ++i)
-    {
-        if (number % i == 0)
-        {
-            return 0;
-        }
-    }
-
-    return 1;
-}
-
 int main()
 {
     int m;
