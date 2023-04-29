c474341f08eac2da5f19d6d2ebc22e22ecc2eefd
DoubleA478 <2040221078@mhs.its.ac.id>
2023-04-29 07:57:28 +0700

    bruh moment

---
+++ praktikum 4/lresmi2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int isPrime(int number)
+{
+    for (int i = 2; i <= number / 2; ++i)
+    {
+        if (number % i == 0)
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int main()
+{
+    int m;
+    printf("program untuk mentotal bilangan prima\n");
+    printf("masukkan nilai m:\n");
+    scanf("%d", &m);
+
+    int count = 0, total = 0, number = 2;
+
+    while (count < m)
+    {
+        int flag = 0;
+
+        for (int i = 2; i <= number / 2; ++i)
+        {
+            if (number % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+        {
+            printf("bilangan prima yang ke %d = %d\n", count + 1, number);
+            total += number;
+            count++;
+        }
+        number++;
+    }
+
+    printf("\n");
+    printf("total %i bil prima pertama = %i\n", m, total);
+
+    return 0;
+}
